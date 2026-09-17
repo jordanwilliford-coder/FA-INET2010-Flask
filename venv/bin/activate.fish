06b578f79cbe3e02b5772961cdf449727d76959b
jordanwilliford-coder <jordanwilliford@sheridan.edu>
2026-09-17 04:22:21 +0000

    week4

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/jwilliford/FA-INET2010-Flask/venv
+set -gx VIRTUAL_ENV /home/jwilliford/microblog/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
